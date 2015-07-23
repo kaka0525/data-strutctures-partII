731f37e273d4040dec59e8360203ead024501f54
Karen Wong <karen.wsywong@gmail.com>
2015-07-22 23:22:12 -0700

    change balance test

--- test_bst.py
+++ test_bst.py
@@ -1,5 +1,5 @@
-from __future__ import literals
-from bst import Tree
+from __future__ import unicode_literals
+from bst import BST
 import pytest
 
 tree_val = [15, 7, 14, 16, 1, 5]
@@ -7,13 +7,13 @@ tree_val = [15, 7, 14, 16, 1, 5]
 
 @pytest.fixture()
 def empty_tree():
-    tree = Tree()
+    tree = BST()
     return tree
 
 
 @pytest.fixture()
 def full_tree():
-    tree = Tree()
+    tree = BST()
     for val in tree_val:
         tree.insert(val)
     return tree
@@ -25,7 +25,7 @@ def test_contains(full_tree):
 
 
 def test_size(full_tree, empty_tree):
-    assert full_tree.size() == tree_val.size()
+    assert full_tree.size() == (len(tree_val))
     assert empty_tree.size() == 0
 
 
@@ -34,12 +34,12 @@ def test_insert(full_tree, empty_tree):
         assert full_tree.contains(val)
     full_tree.insert(22)
     assert full_tree.contains(22)
-    assert full_tree.size() == (tree_val.size + 1)
+    assert full_tree.size() == (len(tree_val) + 1)
     full_tree.insert(22)
-    assert full_tree.size() == (tree_val.size + 1)
+    assert full_tree.size() == (len(tree_val) + 1)
     empty_tree.insert(5)
     assert empty_tree.size() == 1
-    assert empty_tree.contains(1)
+    assert empty_tree.contains(5)
 
 
 def test_depth(full_tree, empty_tree):
@@ -48,13 +48,13 @@ def test_depth(full_tree, empty_tree):
 
 
 def test_balance(full_tree, empty_tree):
-    assert full_tree.balance() < 0
+    assert full_tree.balance() > 0
     assert empty_tree.balance() == 0
     empty_tree.insert(5)
     assert empty_tree.balance() == 0
     empty_tree.insert(8)
-    assert empty_tree.balance() > 0
+    assert empty_tree.balance() < 0
     empty_tree.insert(4)
     assert empty_tree.balance() == 0
     empty_tree.insert(1)
-    assert empty_tree.balance() < 0
+    assert empty_tree.balance() > 0
