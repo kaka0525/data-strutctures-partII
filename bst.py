41b6e93fe5286e4b2fcaa6816b77cbed1a817fae
Karen Wong <karen.wsywong@gmail.com>
2015-08-01 14:07:04 -0700

    there are unmerged files

--- bst.py
+++ bst.py
@@ -28,12 +28,16 @@ class _BstNode(object):  # each node is the root of the subtree
             if self.value > value_to_insert:
                 if self.left:
                     return_val = self.left.insert(value_to_insert)
+                    # After inserting to the left, maintain balance.
+                    self.left = self.left.re_balance()
                 else:
                     self.left = _BstNode(value_to_insert)
                     return_val = True
             else:
                 if self.right:
                     return_val = self.right.insert(value_to_insert)
+                    # After inserting to the right, maintain balance.
+                    self.right = self.right.re_balance()
                 else:
                     self.right = _BstNode(value_to_insert)
                     return_val = True
@@ -54,6 +58,176 @@ class _BstNode(object):  # each node is the root of the subtree
             else:
                 return False
 
+<<<<<<< Updated upstream
+=======
+    def left_rotation(self):
+        old_root = self
+        new_root = old_root.right
+        old_root.right = new_root.left
+        new_root.left = old_root
+        return new_root
+
+    def right_rotation(self):
+        old_root = self
+        new_root = old_root.left
+        old_root.left = new_root.right
+        new_root.right = old_root
+        return new_root
+
+    def double_left_rotation(self):
+        old_root = self
+        old_root.right = old_root.right_rotation()
+        return old_root.left_rotation()
+
+    def double_right_rotation(self):
+        old_root = self
+        old_root.left = old_root.left_rotation()
+        return old_root.right_rotation()
+
+    def re_balance(self):
+        balance = self.balance()
+        if abs(balance) <= 1:
+            # Already balanced; just return self
+            return self
+        elif balance > 1:
+            # Unbalanced to the left
+            left_subtree = self.left
+            if left_subtree.left:
+                return self.right_rotation()
+                # Regular right rotation
+            else:
+                return self.double_left_rotation()
+                # Left-right rotation
+        else:
+            # Unbalanced to the right
+            right_subtree = self.right
+            if right_subtree.right:
+                # Regular left rotation
+                return self.left_rotation()
+            else:
+                # Right-left rotation
+                return self.double_right_rotation()
+
+    def pre_order(self):
+        yield self.value
+        if self.left:
+            for l in self.left.pre_order():
+                yield l
+        if self.right:
+            for r in self.right.pre_order():
+                yield r
+
+    def in_order(self):
+        if self.left:
+            for l in self.left.in_order():
+                yield l
+        yield self.value
+        if self.right:
+            for r in self.right.in_order():
+                yield r
+
+    def post_order(self):
+        if self.left:
+            for l in self.left.post_order():
+                yield l
+        if self.right:
+            for r in self.right.post_order():
+                yield r
+        yield self.value
+
+    def breadth_first(self):
+        q = deque([self])
+        while q:
+            node = q.popleft()
+            yield node.value
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+
+    def _delete_help(self):
+        q = deque([self])
+        while q:
+            node = q.popleft()
+            yield node
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+
+    def delete(self, val):
+        parent_node = None
+        del_node = self
+        nodes = self._delete_help()
+        while nodes:
+            parent_node = nodes.next()
+            if parent_node.value == val:
+                break
+            if parent_node.left is not None:
+                if parent_node.left.value == val:
+                    del_node = parent_node.left
+                    break
+            if parent_node.right is not None:
+                if parent_node.right.value == val:
+                    del_node = parent_node.right
+                    break
+        if del_node.left is not None and del_node.right is not None:
+            temp_node = None
+            if self.balance() > 1:
+                temp_node = del_node.left
+                while temp_node.right is not None:
+                    if temp_node.right.right is None:
+                        unwire = temp_node
+                        temp_node = temp_node.right
+                        unwire.right = None
+                        break
+                    temp_node = temp_node.right
+            else:
+                temp_node = del_node.right
+                while temp_node.left is not None:
+                    if temp_node.left.left is None:
+                        unwire = temp_node
+                        temp_node = temp_node.left
+                        unwire.left = None
+                        break
+                    temp_node = temp_node.left
+            if parent_node == del_node:
+                if self.right == temp_node:
+                    temp_node.left = self.right
+                else:
+                    temp_node.right = self.left
+                self.value = temp_node.value
+            elif parent_node.right == del_node:
+                parent_node.right = temp_node
+                temp_node.left = del_node.left
+                temp_node.right = del_node.right
+            else:
+                parent_node.left = temp_node
+                temp_node.right = del_node.right
+                temp_node.left = del_node.left
+        elif del_node.left is not None:
+            if parent_node == del_node:
+                self.value = self.left.value
+                self.left = self.left.left
+            elif parent_node.left == del_node:
+                parent_node.left = del_node.left
+            else:
+                parent_node.right = del_node.left
+        elif del_node.right is not None:
+            if parent_node == del_node:
+                self.value = self.right.value
+                self.right = self.right.right
+            elif parent_node.left == del_node:
+                parent_node.left = del_node.right
+            else:
+                parent_node.right = del_node.left
+        else:
+            if parent_node.left == del_node:
+                parent_node.left = None
+            else:
+                parent_node.right = None
+
+>>>>>>> Stashed changes
 
 class BST(object):
     def __init__(self):
@@ -74,7 +248,12 @@ class BST(object):
             size_increased = self._root.insert(val)  # value to insert
             if size_increased:
                 self._size += 1
+<<<<<<< Updated upstream
             return size_increased
+=======
+                # Ensure balance at the root
+                self._root = self._root.re_balance()
+>>>>>>> Stashed changes
         else:
             self._root = _BstNode(val)
             self._size += 1
@@ -114,6 +293,52 @@ class BST(object):
         should return 0.
         """
         if self._root:
+<<<<<<< Updated upstream
             return self._root.left_depth() - self._root.right_depth()
         else:
             return 0
+=======
+            return self._root.post_order()
+
+    def breadth_first(self):
+        """
+        Will return a generator that will return the values in the tree using
+        breadth_first traversal, one at a time.
+        """
+        if self._root:
+            return self._root.breadth_first()
+
+    def delete(self, val):
+        if self.contains(val):
+            self._size -= 1
+            return self._root.delete(val)
+
+    def re_balance(self):
+        pass
+
+
+if __name__ == '__main__':
+    def populate(numlist):
+        tree = BST()
+        for num in numlist:
+            tree.insert(num)
+        return tree
+
+    def crappy():
+        numlist = range(1, 10)
+        populate(numlist)
+
+    def ideal():
+        numlist = [5, 2, 8, 1, 3, 7, 9, 6, 10]
+        populate(numlist)
+
+    t0 = time()
+    crappy()
+    crappy_time = time() - t0
+    print "Single List {}".format(crappy_time)
+
+    t0 = time()
+    ideal()
+    ideal_time = time() - t0
+    print "Balanced Tree {}".format(ideal_time)
+>>>>>>> Stashed changes
--- test_bst.py
+++ test_bst.py
@@ -4,6 +4,19 @@ import pytest
 
 tree_val = [15, 7, 14, 16, 1, 5]
 
+<<<<<<< Updated upstream
+=======
+second_tree_val = [15, 7, 16, 5, 14, 3]
+
+right_heavy_tree_val = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+left_heavy_tree_val = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+
+double_left_rotation_tree_val = [10, 12, 11, 10.5]
+
+double_right_rotation_tree_val = [10, 8, 9]
+
+>>>>>>> Stashed changes
 
 @pytest.fixture()
 def empty_tree():
@@ -19,6 +32,24 @@ def full_tree():
     return tree
 
 
+<<<<<<< Updated upstream
+=======
+@pytest.fixture()
+def second_full_tree():
+    tree = BST()
+    for val in second_tree_val:
+        tree.insert(val)
+    return tree
+
+
+@pytest.fixture()
+def right_heavy_tree():
+    tree = BST()
+    for val in right_heavy_tree_val:
+        tree.insert(val)
+    return tree
+
+>>>>>>> Stashed changes
 def test_contains(full_tree):
     for val in tree_val:
         assert full_tree.contains(val)
@@ -58,3 +89,70 @@ def test_balance(full_tree, empty_tree):
     assert empty_tree.balance() == 0
     empty_tree.insert(1)
     assert empty_tree.balance() > 0
+<<<<<<< Updated upstream
+=======
+
+
+def test_pre_order(full_tree):
+    g = full_tree.pre_order()
+    assert list(g) == [15, 7, 1, 5, 14, 16]
+
+
+def test_in_order(full_tree):
+    g = full_tree.in_order()
+    assert list(g) == [1, 5, 7, 14, 15, 16]
+
+
+def test_post_order(full_tree):
+    g = full_tree.post_order()
+    assert list(g) == [5, 1, 14, 7, 16, 15]
+
+
+def test_breadth_first(full_tree):
+    g = full_tree.breadth_first()
+    assert list(g) == [15, 7, 16, 1, 14, 5]
+
+
+def test_delete_leaf(full_tree):
+    full_tree.delete(16)
+    assert full_tree.size() == 5
+    g = full_tree.in_order()
+    assert list(g) == [1, 5, 7, 14, 15]
+    assert full_tree.contains(16) is False
+
+
+def test_delete_node_with_one_right_child(full_tree):
+    full_tree.delete(1)
+    assert full_tree.size() == 5
+    g = full_tree.in_order()
+    assert list(g) == [5, 7, 14, 15, 16]
+    assert full_tree.contains(1) is False
+
+
+def test_delete_node_with_one_left_child(second_full_tree):
+    second_full_tree.delete(5)
+    assert second_full_tree.size() == 5
+    g = second_full_tree.in_order()
+    assert list(g) == [3, 7, 14, 15, 16]
+    assert second_full_tree.contains(5) is False
+
+
+def test_delete_node_with_two_child(full_tree):
+    full_tree.delete(7)
+    assert full_tree.size() == 5
+    g = full_tree.in_order()
+    assert list(g) == [1, 5, 14, 15, 16]
+    assert full_tree.contains(7) is False
+
+
+def test_delete_root_node(full_tree):
+    full_tree.delete(15)
+    assert full_tree.size() == 5
+    g = full_tree.in_order()
+    assert list(g) == [1, 5, 7, 14, 16]
+    assert full_tree.contains(15) is False
+
+
+def test_balance_right_heavy_tree(right_heavy_tree):
+    assert right_heavy_tree.size() == 10
+>>>>>>> Stashed changes
