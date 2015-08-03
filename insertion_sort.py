aa14139b4c48b276fa6040a9606c860ccc06e652
Karen Wong <karen.wsywong@gmail.com>
2015-08-03 14:46:13 -0700

    add insertion sort

---
+++ insertion_sort.py
@@ -0,0 +1,36 @@
+from __future__ import unicode_literals
+from time import time
+
+
+def insertion_sort(list):
+    for index in range(1, len(list)):
+        value = list[index]
+        i = index - 1
+        while i > 0:
+            if value < list[i]:
+                list[i + 1] = list[i]
+                list[i] = value
+                i = i - 1
+            else:
+                break
+
+
+if __name__ == '__main__':
+    def worst_case():
+        list_a = range(100)
+        list_a = list_a.reverse()
+        return list_a
+
+    def best_case():
+        list_b = range(100)
+        return list_b
+
+t0 = time()
+worst_case()
+worst_time = time() - t0
+print "A reverse list {} perfomance with insertion sort ".format(worst_time)
+
+t0 = time()
+best_case()
+best_time = time() - t0
+print "A sorted list {} perfomance with insertion sort".format(best_time)
