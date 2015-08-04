6c6785fd1d4eb0c1306fc5b2d19928e9df2f85fc
Karen Wong <karen.wsywong@gmail.com>
2015-08-04 14:36:07 -0700

    remove the merge sort function from the insertion sort file

--- sort.py
+++ sort.py
@@ -16,35 +16,6 @@ def insertion_sort(init_list):
                 break
     return return_list
 
-
-def merge_sort(init_list):
-    if len(init_list) <= 1:
-        return init_list
-    list_left = init_list[:len(init_list)/2]
-    list_right = init_list[len(init_list)/2:]
-    left = merge_sort(list_left)
-    right = merge_sort(list_right)
-    return merge(left, right)
-
-
-def merge(left, right):
-    return_list = []
-    while left and right:
-        if left[0] <= right[0]:
-            return_list.append(left[0])
-            left = left[1:]
-        else:
-            return_list.append(right[0])
-            right = right[1:]
-    while left:
-        return_list.append(left[0])
-        left = left[1:]
-    while right:
-        return_list.append(right[0])
-        right = right[1:]
-    return return_list
-
-
 if __name__ == '__main__':
     def worst_case():
         list_a = range(100)
