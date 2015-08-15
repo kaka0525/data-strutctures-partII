f02eb476b08280c7f2cb6ea0fde20deda33972fd
johnsonjjew@gmail.com <johnsonjjew@gmail.com>
2015-08-15 11:25:15 -0700

    fixed merge conflicts

--- insertion_sort.py
+++ insertion_sort.py
@@ -27,31 +27,19 @@ if __name__ == '__main__':
         return list_b
 
     iteration_list = [10, 100, 1000, 10000]
-<<<<<<< HEAD
-    random_list = []
-    sorted_list = []
-    for iteration in iteration_list:
-        random_list.append(worst_case(iteration))
-        sorted_list.append(best_case(iteration))
-=======
     random_list = [[] for x in range(4)]
     sorted_list = [[] for x in range(4)]
 
     for i in range(len(iteration_list)):
         random_list[i].extend(worst_case(iteration_list[i]))
         sorted_list[i].extend(best_case(iteration_list[i]))
->>>>>>> 60f0f978c1f87de7a79e3b1469875c810292cae0
 
     count = 0
     for test in random_list:
         t0 = time()
         insertion_sort(test)
         worst_time = time() - t0
-<<<<<<< HEAD
-        print "A reverse list with {} entries, takes {} seconds with merge sort"\
-=======
         print "A reverse list with {} entries, takes {} seconds with insertion sort"\
->>>>>>> 60f0f978c1f87de7a79e3b1469875c810292cae0
             .format(iteration_list[count], worst_time)
         count += 1
 
@@ -60,10 +48,7 @@ if __name__ == '__main__':
         t0 = time()
         insertion_sort(test)
         worst_time = time() - t0
-<<<<<<< HEAD
-        print "An already sorted list with {} entries, takes {} seconds with merge sort"\
-=======
+
         print "An already sorted list with {} entries, takes {} seconds with insertion sort"\
->>>>>>> 60f0f978c1f87de7a79e3b1469875c810292cae0
             .format(iteration_list[count], worst_time)
         count += 1
